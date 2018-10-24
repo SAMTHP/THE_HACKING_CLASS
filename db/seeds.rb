ffae1c841aecc70fbaabf8f4d37787c4e1f12586
Vano <hervelee99@gmail.com>
2018-10-24 19:54:45 +0200

    ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  cour = Cour.create!(name: Faker::Company.name)
+end
+
+10.times do
+  eleve = Student.create!(name: Faker::Company.name,cour_id: rand(1..10))
+end
